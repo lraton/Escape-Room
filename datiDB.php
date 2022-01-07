6c4e472ab246aefaa54e9a991e346a04ab3fa295
Iraton <50119007+lraton@users.noreply.github.com>
2022-01-07 15:35:16 +0100

    Delete datiDB.php

--- datiDB.php
+++
@@ -1,6 +0,0 @@
-<?php
-$mysql_host = ""; // nome del server su cui gira MySQL
-$mysql_user = ""; // nome utente MySQL
-$mysql_password = ""; // password MySQL
-$mysql_database = ""; //nome del database
-?>
